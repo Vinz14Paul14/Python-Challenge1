2e7ff7b751e20c71f88a2bc383242fcba61af276
Paul Pineda <pinedavinz@gmail.com>
2020-03-23 17:43:58 -0700

    updated

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,13 +16,29 @@ with open(election_data) as csvfile:
     print(csv_header)
     
     candidate_names = []
+    vote_count = {}
 
     for abc in csvreader:
         if abc[2] not in candidate_names: 
             candidate_names.append(abc[2])
-
+            vote_count[abc[2]] = 1
+        vote_count[abc[2]] = vote_count[abc[2]] + 1
         total_votes = total_votes + 1
 
     print(total_votes)
     print(candidate_names)
+    print(vote_count)
+
+    winner = ""
+    max_votes = 0
+    for abc in vote_count:
+        votes = vote_count.get(abc)
+        if votes > max_votes :
+            max_votes = votes
+            winner = abc 
+        print(abc,votes)
+        total_percentage = float((votes / total_votes) * 100)
+        print(total_percentage)
+        print(f"{total_percentage:.2f}%")
+        print("The Winner is " + winner + " "+ str(max_votes))
 
